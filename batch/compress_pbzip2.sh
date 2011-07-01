686f4d7453face324631c083b49a98b926087326
Roman Valls Guimera <roman.valls.guimera@scilifelab.se>
2011-07-01 16:21:04 +0200

    Decompress on SLURM batch too

--- batch/compress_pbzip2.sh
+++ batch/compress_pbzip2.sh
@@ -3,7 +3,7 @@
 #SBATCH -N 1
 #SBATCH -t 5:00:00
 #SBATCH -J pbzip2
-#SBATCH -A YOUR_PROJECT_OR_QUEUE
+#SBATCH -A project_id 
 #SBATCH --mail-user=YOURMAIL@example.com
 #SBATCH --mail-type=ALL
 
@@ -12,22 +12,28 @@
 # ... using all cores with pbzip2
 
 function usage {
-    echo "Usage: compress_pbzip2.sh [dir|file]"
-    echo "Output: file.bz2 if it's a single file or dir.tar.bz2"
+    echo "Usage: compress_pbzip2.sh <-d> [dir|file]"
+    echo "Output: file.bz2 if it's a single file or dir.tar.bz2, -d for decompressing"
 }
+
+if [ $1 == "-d" ]; then
+	pbzip2 -d $2
+	exit
+fi
 
 if [ ! -e "$1" ]; then
 	usage && exit
 fi
 
+
 if [ -d $1 ]
 then
-    tar cf $1.tar.bz2 --use-compress-prog=$HOME/bin/pbzip2 $1
+    tar cf $1.tar.bz2 --use-compress-prog=pbzip2 $1
 elif [ -f $1 ]
 then
 # In this case, it bypasses core autodetection and sets 16 cores
 # (16 "virtual cores" by HyperThreading):
-    $HOME/bin/pbzip2 -p16 $1
+    pbzip2 -p16 $1
 else
     usage
 fi
