00e8b393f903444d65b153aae7d313fa4ed774a4
b97pla <pontus.larsson@scilifelab.se>
2011-09-08 08:55:59 +0200

    Changed to pontusla environment

--- batch/nosetests_run.sh
+++ batch/nosetests_run.sh
@@ -4,8 +4,8 @@
 #SBATCH -t 00:30:00
 #SBATCH -J bcbb_testsuite
 #SBATCH -A a2010002
-#SBATCH -D /bubo/home/h5/roman/opt/bcbb/nextgen/tests
-#SBATCH --mail-user=roman.valls.guimera@scilifelab.se
+#SBATCH -D /bubo/home/h27/pontusla/bcbb/nextgen/tests
+#SBATCH --mail-user=pontus.larsson@scilifelab.se
 #SBATCH --mail-type=ALL
 #SBATCH -o nosetest_job.out
 #SBATCH -e nosetest_job.err
