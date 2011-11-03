a24c509dd58460c670bbce824ecdff27794ed7f9
Roman Valls Guimera <roman.valls.guimera@scilifelab.se>
2011-11-03 11:10:56 +0100

    pbzip2 compression was broken, thanks @percyfal for the heads up

--- batch/compress_pbzip2.sh
+++ batch/compress_pbzip2.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH -p node
 #SBATCH -N 1
-#SBATCH -t 5:00:00
+#SBATCH -t 10:00:00
 #SBATCH -J pbzip2
-#SBATCH -A project_id 
-#SBATCH --mail-user=YOURMAIL@example.com
+#SBATCH -A a2010002
+#SBATCH --mail-user=roman.valls.guimera@scilifelab.se
 #SBATCH --mail-type=ALL
 
 # Compress everything:
@@ -12,23 +12,25 @@
 # ... using all cores with pbzip2
 
 function usage {
-    echo "Usage: compress_pbzip2.sh <-d> [dir|file]"
+    echo "Usage: $0 <-d> <-s> [dir|file] <dest_dir>"
+    echo "Example: sbatch compress_slurm.sh bigfolder /store/longterm/mount/"
     echo "Output: file.bz2 if it's a single file or dir.tar.bz2, -d for decompressing"
 }
-
-if [ $1 == "-d" ]; then
-	pbzip2 -d $2
-	exit
-fi
 
 if [ ! -e "$1" ]; then
 	usage && exit
 fi
 
+# Decompress
+if [ $1 == "-d" ]; then
+	tar xf $2.tar.bz2 --use-compress-prog=pbzip2
+	exit
+fi
 
+# If its a directory, tar it too
 if [ -d $1 ]
 then
-    tar cf $1.tar.bz2 --use-compress-prog=pbzip2 $1
+    tar cf $2`basename $1`.tar.bz2 --use-compress-prog=pbzip2 $1
 elif [ -f $1 ]
 then
 # In this case, it bypasses core autodetection and sets 16 cores
