d461e5b2af8e57cdd312cdc1a1df14ebaa87742b
Valentine Svensson <valentine@nxn.se>
2011-12-22 15:56:22 +0100

    Finished the plotting script. It makes a cumulative bar plot of sizes of runs over time. Path to the .log file should be pointed correctly when used. Same goes for where the pdf is saved.

--- scripts/datasets.py
+++ scripts/datasets.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 
 import matplotlib.pyplot as plt
+import itertools
+from operator import itemgetter
+import datetime
+from collections import defaultdict
 
 
 def convert_human_readable_to_numbers(file_size):
@@ -21,6 +25,8 @@ def convert_human_readable_to_numbers(file_size):
 
     return file_size
 
+# Get the data and structure it
+
 with open("../sizegraphing/datasets_sizes.log") as data_f:
     all_data = data_f.read()
 
@@ -32,17 +38,55 @@ for line in data:
 data = [line for line in data if "." not in line[-1]]
 sizes = [int(convert_human_readable_to_numbers(line[0])) for line in data]
 data = [line[-1].split("_") for line in data]
+
 for i in range(len(sizes)):
     data[i].insert(0, sizes[i])
 
-N = len(sizes)
+data = [line for line in data if len(line) == 5]
+
+for i in range(len(data)):
+    data[i][3:5] = ["_".join(data[i][3:5])]
+    data[i][1] = datetime.datetime.strptime(data[i][1], "%y%m%d")
+
+for line in data:
+    line[2] = line[2].upper()
+    line[3] = line[3].upper()
+
+data = sorted(data, key=itemgetter(3))
+grouped_data = [list(g) for k, g in itertools.groupby(data, itemgetter(3))]
+
+sorted_grouped_data = []
+for group in grouped_data:
+    sorted_grouped_data.append(sorted(group, key=itemgetter(0)))
+
+# Modify data for plotting (increment dates with the days past since creation)
+
+for i in range(len(sorted_grouped_data)):
+    for j in range(len(sorted_grouped_data[i])):
+        sorted_grouped_data[i][j][1] += datetime.timedelta(days=j)
+
+# Plot
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
-num_values = 75
-ax.bar(range(num_values), sizes[:num_values])
+colors = ["b", "g", "r", "c", "m", "y"]
+i = 0
+stack_heights = defaultdict(int)
+for group in sorted_grouped_data:
+    clr = colors[i % 6]
+    i += 1
+    sizes = [line[0] for line in group]
+    days = [line[1] for line in group]
+    heights = []
+    for day in days:
+        heights.append(stack_heights[day])
+
+    ax.bar(days, sizes, bottom=heights, color=clr, width=1.0, lw=0.01)
+
+    for size, day in zip(sizes, days):
+        stack_heights[day] += size
 
 plt.ylabel('Sizes')
 ax.set_title('Dataset sizes')
 
-plt.show()
+fig.savefig("../sizegraphing/stacked_sizes.pdf")
