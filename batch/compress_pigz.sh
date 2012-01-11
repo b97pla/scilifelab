786195c43463131ff085365bc76222be9147ef36
b97pla <pontus.larsson@scilifelab.se>
2012-01-11 12:34:19 +0100

    pipeline run script, run_info process script and pigz modification

--- batch/compress_pigz.sh
+++ batch/compress_pigz.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 #SBATCH -p node
 #SBATCH -N 1
-<<<<<<< HEAD
 #SBATCH -t 5:00:00
 #SBATCH -J pigz
 #SBATCH -A a2010002
 #SBATCH --mail-user=pontus.larsson@scilifelab.se
-=======
-#SBATCH -t 10:00:00
-#SBATCH -J pbzip2
-#SBATCH -A a2010002
-#SBATCH --mail-user=roman.valls.guimera@scilifelab.se
->>>>>>> c9e32f980979c53142245d06367f6efc2b05d485
 #SBATCH --mail-type=ALL
 
 # Compress everything:
@@ -24,32 +17,19 @@ function usage {
     echo "Output: file.bz2 if it's a single file or dir.tar.bz2, -d for decompressing"
 }
 
-<<<<<<< HEAD
-if [ $1 == "-d" ]; then
-	pigz -d $2
-	exit
-fi
-
-=======
->>>>>>> c9e32f980979c53142245d06367f6efc2b05d485
 if [ ! -e "$1" ]; then
 	usage && exit
 fi
 
-# Decompress
 if [ $1 == "-d" ]; then
-	tar xf $2.tar.bz2 --use-compress-prog=pbzip2
+	pigz -d $2
 	exit
 fi
 
 # If its a directory, tar it too
 if [ -d $1 ]
 then
-<<<<<<< HEAD
     tar cf $1.tar.bz2 --use-compress-prog=pigz $1
-=======
-    tar cf $2`basename $1`.tar.bz2 --use-compress-prog=pbzip2 $1
->>>>>>> c9e32f980979c53142245d06367f6efc2b05d485
 elif [ -f $1 ]
 then
 # In this case, it bypasses core autodetection and sets 16 cores
---
+++ scripts/process_run_info.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+import os
+import sys
+import yaml
+from optparse import OptionParser
+
+def main(run_info_yaml, lane, out_file, genome_build, barcode_type, trim, ascii, analysis, description, verbose):
+    
+    if verbose: print "Verifying that %s exists" % run_info_yaml
+    assert os.path.exists(run_info_yaml)
+    if verbose: print "Parsing %s" % run_info_yaml
+    with open(run_info_yaml) as fh:
+        run_info = yaml.load(fh)
+
+    if verbose: print "Extracting lane info"
+    if lane == 0:
+        lane_info = run_info
+    else:
+        for info in run_info:
+            if (int(info.get("lane",0)) == lane):
+                lane_info = [info]
+                break
+    for info in lane_info:
+        if verbose: print "Processing lane %s" % info["lane"]
+        _process_info(info,genome_build,barcode_type,trim,ascii,analysis,description,verbose)
+    
+    if out_file is not None:
+        with open(out_file,'w') as fh:
+            yaml.dump(run_info, fh, allow_unicode=True, default_flow_style=False)
+    else:
+        print yaml.dump(run_info, allow_unicode=True, default_flow_style=False)
+        
+        
+def _process_info(info,genome_build,barcode_type,trim,ascii,analysis,description,verbose):
+    
+    if genome_build is not None:
+        if verbose: print "\tSetting genome build: %s" % genome_build
+        info['genome_build'] = genome_build
+    if analysis is not None:
+        if verbose: print "\tSetting analysis: %s" % analysis
+        info['analysis'] = analysis
+    if description is not None:
+        if verbose: print "\tSetting description: %s" % description
+        info['description'] = description
+    if ascii and 'description' in info:
+        if verbose: print "\tEnsuring ascii"
+        info['description'] = _replace_ascii(info['description'])
+        
+    for multiplex in info.get('multiplex',[]):
+        if verbose: print "\tProcessing multiplexed barcode id %s" % multiplex['barcode_id']
+        if barcode_type is not None:
+            if verbose: print "\t\tSetting barcode_type: %s" % barcode_type
+            multiplex['barcode_type'] = barcode_type
+        if trim > 0:
+            if verbose: print "\t\tTrimming %s nucleotides from end of barcode" % trim
+            multiplex['sequence'] = multiplex['sequence'][0:(-1*trim)]
+        if ascii and 'description' in multiplex:
+            if verbose: print "\t\tEnsuring ascii"
+            multiplex['description'] = _replace_ascii(multiplex['description'])
+    
+def _replace_ascii(str):
+    # Substitute swedish characters for sensible counterparts
+    str = str.replace(u'\xc5','A')
+    str = str.replace(u'\xe5','a')
+    str = str.replace(u'\xc4','A')
+    str = str.replace(u'\xe4','a')
+    str = str.replace(u'\xd6','O')
+    str = str.replace(u'\xf6','o') 
+    return str.encode('ascii','replace')
+ 
+if __name__ == "__main__":
+    parser = OptionParser()
+    parser.add_option("-l", "--lane", dest="lane", default=0)
+    parser.add_option("-o", "--out_file", dest="out_file", default=None)
+    parser.add_option("-g", "--genome_build", dest="genome_build", default=None)
+    parser.add_option("-b", "--barcode_type", dest="barcode_type", default=None)
+    parser.add_option("-t", "--trim", dest="trim", default=0)
+    parser.add_option("-a", "--analysis", dest="analysis", default=None)
+    parser.add_option("-d", "--description", dest="description", default=None)
+    parser.add_option("-i", "--ascii", dest="ascii", default=False, \
+                                                        action="store_true")
+    parser.add_option("-v", "--verbose", dest="verbose", default=False, \
+                                                        action="store_true")
+    options, args = parser.parse_args()
+    if len(args) == 1:
+        run_info_yaml, = args
+    else:
+        print __doc__
+        sys.exit()
+
+    main(run_info_yaml, int(options.lane), options.out_file, 
+            options.genome_build, options.barcode_type, int(options.trim), \
+            options.ascii, options.analysis, options.description, options.verbose)
+    
