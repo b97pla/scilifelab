b20fb33bcdf8113620526da9eac32bf0fba6a453
hussius <mikael.huss@gmail.com>
2012-06-07 14:56:10 +0200

    Cosmetic changes

--- utilities/count_barcodes.py
+++ utilities/count_barcodes.py
@@ -66,27 +66,17 @@ last_was_header = False
 pos = int(sys.argv[2])
 lgth = int(sys.argv[3])
 
-if gz:
-    for line in gzip.open(sys.argv[1]):
-        if not last_was_header:
-            if line[0]=="@": last_was_header=True
-            continue
-
-        bcode = line[pos:(pos+lgth)].strip()
-        if bcodes.has_key(bcode): bcodes[bcode] += 1
-        else: bcodes[bcode] = 1
-        if last_was_header: last_was_header = False
-
-else:
-    for line in open(sys.argv[1]):
-        if not last_was_header:
-            if line[0]=="@": last_was_header=True
-            continue
-
-        bcode = line[pos:(pos+lgth)].strip()
-        if bcodes.has_key(bcode): bcodes[bcode] += 1
-        else: bcodes[bcode] = 1
-        if last_was_header: last_was_header = False
+if gz: fh = gzip.open(sys.argv[1])
+else: fh = open(sys.argv[1])
+    
+for line in fh: 
+    if not last_was_header:
+        if line[0]=="@": last_was_header=True
+        continue
+    bcode = line[pos:(pos+lgth)].strip()
+    if bcodes.has_key(bcode): bcodes[bcode] += 1
+    else: bcodes[bcode] = 1
+    if last_was_header: last_was_header = False
 
 for e in sorted(bcodes.items(), key=itemgetter(1)):
     illum = '(no exact match to Illumina)'
