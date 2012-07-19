2dde0ab02a3c1a5e7323d5c07b9f946f9f50f78d
Roman Valls Guimera <roman.valls.guimera@scilifelab.se>
2012-07-19 17:33:48 +0200

    Building package for scilifelab, including @percyfal pmtools

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.0'
+
+setup(name='scilifelab',
+      version=version,
+      description="SciLifeLab life sciences facility platform python scripts.",
+      long_description="""Science For Life Laboratory python scripts""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='scilifelab science',
+      author='Science for Life Lab staff',
+      author_email='roman@scilifelab.se',
+      url='http://scilifelab.se',
+      license='GPLv3',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          "cement"
+      ],
+      entry_points={
+        'console_scripts': [
+            'pm = scilifelab.bin.pm'
+         ]
+      }
+   )
