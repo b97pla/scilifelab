433a4782e3d22ea80e3891359af2c99454d10f44
Per Unneberg <per.unneberg@scilifelab.se>
2012-09-14 20:50:47 +0200

    Add run command for pipeline analyses

--- project_management/pmtools/core/project.py
+++ project_management/pmtools/core/project.py
@@ -45,6 +45,7 @@ Code
 import os
 import sys
 import re
+import yaml
 
 from cement.core import controller, hook
 from pmtools.core.controller import AbstractExtendedBaseController
@@ -61,9 +62,12 @@ class ProjectController(AbstractExtendedBaseController):
         arguments = [
             (['project'], dict(help="Scilife project id (e.g. j_doe_00_00)", default="", action="store", nargs="?")),
             (['-g', '--git'], dict(help="Initialize git directory in repos and project gitdir", default=False, action="store_true")),
-            (['-S', '--sample'], dict(help="project sample id", action="store")),
-            (['-F', '--flowcell'], dict(help="project flowcell id", action="store")),
+            (['-S', '--sample'], dict(help="project sample id", action="store", default=None)),
+            (['-F', '--flowcell'], dict(help="project flowcell id", action="store", default=None)),
             (['--finished'], dict(help="include finished project listing", action="store_true", default=False)),
+            (['--analysis_type'], dict(help="set analysis ", action="store", default=None, type=str)),
+            (['--genome_build'], dict(help="genome build ", action="store", default="hg19", type=str)),
+            (['--post_process'], dict(help="post process file", action="store", default=None, type=str)),
             ]
         flowcelldir = None
 
@@ -81,12 +85,12 @@ class ProjectController(AbstractExtendedBaseController):
             self._meta.path_id = self.pargs.project
         super(ProjectController, self)._process_args()
 
-    ## utility functions
-    def _assert_project_id(self, msg):
-        if not self.pargs.project:
-            self.log.warn(msg)
-            return False
-        return True
+    # ## utility functions
+    # def _assert_project_id(self, msg):
+    #     if not self.pargs.project:
+    #         self.log.warn(msg)
+    #         return False
+    #     return True
 
     ## init
     @controller.expose(help="Initalize project folder")
@@ -125,10 +129,36 @@ class ProjectController(AbstractExtendedBaseController):
         
     ## NOTE: this is a temporary workaround for cases where data has
     ## been removed from analysis directory
-    @controller.expose(help="Transfer project data")
+    @controller.expose(help="Transfer project data to customer. Temporary fix for cases where data has been removed from analysis directory.")
     def transfer(self):
         if not self.pargs.flowcell:
             self.log.warn("No flowcellid provided. Please provide a flowcellid from which to deliver. Available options are:\n\t{}".format("\n\t".join(self._flowcells())))
             return
         
+    @controller.expose(help="run automated initial analysis on one or all samples in a project")
+    def run(self):
+        if not self._check_pargs(["project", "post_process"]):
+            return
+        ## Gather sample yaml files
+        pattern = "-bcbb-config.yaml$"
+        if self.pargs.sample:
+            pattern = "{}{}".format(self.pargs.sample, pattern)
+        def bcbb_yaml_filter(f):
+            return re.search(pattern, f) != None
+        flist = filtered_walk(os.path.join(self._meta.project_root, self.pargs.project, "data"), bcbb_yaml_filter)
+        for f in flist:
+            with open(f) as fh:
+                config = yaml.load(fh)
+            if self.pargs.analysis_type:
+                config["details"][0]["multiplex"][0]["analysis"] = self.pargs.analysis_type
+                config["details"][0]["analysis"] = self.pargs.analysis_type
+            if config["details"][0]["genome_build"] == 'unknown':
+                config["details"][0]["genome_build"] = self.pargs.genome_build
+            config_file = f.replace("-bcbb-config.yaml", "-pm-bcbb-analysis-config.yaml")
+            self.app.cmd.write(config_file, yaml.dump(config))
+            ## Run automated_initial_analysis.py
+            self.app.cmd.command(['automated_initial_analysis.py', os.path.abspath(self.pargs.post_process), os.path.abspath(os.path.dirname(f)), config_file])
 
+## FIXME: analysis should be a separate controller that deals with
+## best practice etc. The current analysis should be renamed to
+## production.
