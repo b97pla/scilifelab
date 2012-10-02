5e02725e74af5725d88a172421b55f08105a1363
b97pla <pontus.larsson@scilifelab.se>
2012-10-02 21:21:39 +0200

    moved slurm methods to module

--- tests/test_status_query.py
+++ tests/test_status_query.py
@@ -262,22 +262,6 @@ class TestMetadata(unittest.TestCase):
         self.assertFalse(sq._fastq_screen_finished(self.rootdir),
                          "Fastq screen should not be considered finished with non-empty output file but without corresponding png")
         
-    def test__get_slurm_jobid(self):
-        """Test that the jobid for a slurm job name can be extracted
-        """
-        
-        # Mock the system calls
-        subprocess.check_output = Mock(return_value='')
-        # Assert that getting non-existing jobs return an empty job list
-        self.assertListEqual([],sq._get_slurm_jobid("jobname"),
-                             "Querying for jobid of non-existing job should return an empty list")
-        # Assert that a returned job id is parsed correctly
-        for jobids in [[123456789],[123456789,987654321]]:
-            subprocess.check_output = Mock(return_value="\n".join([str(jid) for jid in jobids]))
-            self.assertListEqual(jobids,sq._get_slurm_jobid("jobname"),
-                                 "Querying for jobid of existing job did not return the correct value")
-        
-        
         
         
         
