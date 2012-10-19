7ef37901556c1e25e90fbc30c6ccdb856efb7d6b
Per Unneberg <per.unneberg@scilifelab.se>
2012-10-19 07:11:02 +0200

    Add chdir in test setUp

--- tests/full/test_production.py
+++ tests/full/test_production.py
@@ -12,15 +12,19 @@ LOG = logbook.Logger(__name__)
 JDOE01 = os.path.abspath(os.path.join(os.curdir, "data", "production", "J.Doe_00_01"))
 JDOE04 = os.path.abspath(os.path.join(os.curdir, "data", "production", "J.Doe_00_04"))
 
+filedir = os.path.abspath(os.curdir)
+
 class ProductionTest(PmFullTest):
     def setUp(self):
+        if not os.getcwd() == filedir:
+            os.chdir(filedir)
         LOG.info("Copy tree {} to {}".format(JDOE01, JDOE04))
         if not os.path.exists(JDOE04):
             shutil.copytree(JDOE01, JDOE04)
 
-    # def tearDown(self):
-    #     LOG.info("Removing directory tree {}".format(JDOE04))
-    #     shutil.rmtree(JDOE04)
+    def tearDown(self):
+        LOG.info("Removing directory tree {}".format(JDOE04))
+        shutil.rmtree(JDOE04)
 
     def test_production_setup(self):
         self.app = self.make_app(argv = ['production', 'run', 'J.Doe_00_04', '--debug', '--force', '--only_setup'])
