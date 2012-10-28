eac8780f495cb527eb895f9f7bbf32cfb158299f
Per Unneberg <per.unneberg@scilifelab.se>
2012-10-28 18:53:01 +0100

    Emphasize missing pattern in log message

--- scilifelab/pm/core/controller.py
+++ scilifelab/pm/core/controller.py
@@ -207,7 +207,7 @@ class AbstractExtendedBaseController(AbstractBaseController):
 
         flist = filtered_walk(os.path.join(self._meta.root_path, self._meta.path_id), clean_filter, include_dirs=self._meta.include_dirs)
         if len(flist) == 0:
-            self.app.log.info("No files matching pattern {} found".format(pattern))
+            self.app.log.info("No files matching pattern '{}' found".format(pattern))
             return
         if len(flist) > 0 and not query_yes_no("Going to remove {} files ({}...). Are you sure you want to continue?".format(len(flist), ",".join([os.path.basename(x) for x in flist[0:10]])), force=self.pargs.force):
             return
@@ -227,7 +227,7 @@ class AbstractExtendedBaseController(AbstractBaseController):
             flist = filtered_walk(os.path.join(self._meta.root_path, self._meta.path_id), compress_filter)
 
         if len(flist) == 0:
-            self.app.log.info("No files matching pattern {} found".format(pattern))
+            self.app.log.info("No files matching pattern '{}' found".format(pattern))
             return
         if len(flist) > 0 and not query_yes_no("Going to {} {} files ({}...). Are you sure you want to continue?".format(label, len(flist), ",".join([os.path.basename(x) for x in flist[0:10]])), force=self.pargs.force):
             sys.exit()
