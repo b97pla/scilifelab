a3faee86cd4fb027c27bb8af9bd4fcbf4282503e
Per Unneberg <per.unneberg@scilifelab.se>
2012-11-20 17:26:41 +0100

    Fix statusdb tests

--- tests/statusdb/test_documents.py
+++ tests/statusdb/test_documents.py
@@ -10,15 +10,10 @@ LOG = logbook.Logger(__name__)
 class TestDocuments(unittest.TestCase):
     def test_doc(self):
         fc = flowcell_run_metrics(fc_name="Test", fc_date="121101")
-        print fc
-        print dir(fc)
-        print json.dumps(fc)
-        print fc["entity_type"]
-        print repr(fc)
+        self.assertEqual(fc["name"], "121101_Test")
+        self.assertEqual(fc["entity_type"], "flowcell_run_metrics")
         ps = project_summary()
-        print ps
-        print json.dumps(ps)
+        self.assertEqual(ps["entity_type"], "project_summary")
         kw = {'name':'test', 'barcode_name':'noeu'}
         srm = sample_run_metrics(**kw)
-
-        print json.dumps(srm)
+        self.assertEqual(srm["name"], "None_None_None_None")
