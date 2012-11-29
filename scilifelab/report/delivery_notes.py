17d574a4a1e965d6b1073d2f0edf41c9606412d3
Per Unneberg <per.unneberg@scilifelab.se>
2012-11-29 10:43:40 +0100

    Move format_barcode_count to section after assessment of run success. Fixes #108

--- scilifelab/report/delivery_notes.py
+++ scilifelab/report/delivery_notes.py
@@ -177,7 +177,6 @@ def sample_status_note(project_id=None, flowcell=None, username=None, password=N
             s_param["ordered_amount"] = _get_ordered_million_reads(s["barcode_name"], ordered_million_reads)
         if bc_count:
             s_param["rounded_read_count"] = _get_bc_count(s["barcode_name"], bc_count)
-        s_param['rounded_read_count'] = _format_read_count(s_param['rounded_read_count'])# 
         if uppnex_id:
             s_param["uppnex_project_id"] = uppnex_id
         if customer_reference:
@@ -204,6 +203,7 @@ def sample_status_note(project_id=None, flowcell=None, username=None, password=N
             s_param['customer_name'] = None
             LOG.warn("No project sample name found for sample run name '{}'".format(s["barcode_name"]))
         s_param['success'] = sequencing_success(s_param, cutoffs)
+        s_param['rounded_read_count'] = _format_read_count(s_param['rounded_read_count'])
         s_param.update({k:"N/A" for k in s_param.keys() if s_param[k] is None or s_param[k] ==  ""})
         if sample_count[s.get("barcode_name")] > 1:
             outfile = "{}_{}_{}_{}.pdf".format(s["barcode_name"], s["date"], s["flowcell"], s["lane"])
