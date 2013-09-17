e9f9e3825d686e800916a21525c6f2b49d4e9c1c
Jun Wang <junw@biologin.uppmax.uu.se>
2013-09-17 22:38:10 +0200

    update the script

--- scripts/sanity_check.py
+++ scripts/sanity_check.py
@@ -3,87 +3,72 @@
 import sys
 import os
 import gzip
-import random
-import optparse
-from Bio import SeqIO
+import argparse
 from Bio.Blast.Applications import NcbiblastnCommandline
 from Bio.Blast import NCBIXML
 from operator import itemgetter, attrgetter
 
-usage="""
-    Check if there are any significant contributions from unexpected organisms.
-    The script screens the subset of reads against the Genebank database by using BLAST version 2.2.27+ (blast/2.2.27+)
 
-Usage:
-        sanity_check.py <*.fastq.gz or *.fastq>
-    The first option is mandatory, and there are further flages you can use:
+parser = argparse.ArgumentParser(description="Check if there are any significant contributions from unexpected organisms. " \
+    "The script screens the subset of reads against the Genebank database by using BLAST version 2.2.27+ (blast/2.2.27+). " \
+    "Either a gzip compressed or uncompressed fastq file is requierd. " \
+    "Results are placed in the subdirectory (sanity_check) under the current folder. ")
 
--s, --subset: number of reads for the subset (default=5000)
--d, --discard: number of reads discard from the top (default=25000)
--t, --num_threads: number of threads (CPUs) to use in the BLAST search (default=1)
--n, --nhit: number of top hits to report (default=50)
+parser.add_argument('-s', '--subset', dest="subset", default="5000", help="Number of reads for the subset (default=5000)")
+parser.add_argument('-d', '--discard', dest="startFq", default="25000", help="Number of reads discard from the top (default=25000)")
+parser.add_argument('-t', '--num_threads',  dest="threads", default="1", help="Number of threads (CPUs) to use in the BLAST search (default=1)")
+parser.add_argument('-n', '--nhit',  dest="nhit", default="50", help="Number of top hits to report (default=50)")
+parser.add_argument('file_name', help="Fastq file name, e.g. P286_109B_index2_CGATGT_L001_R1_001.fastq.gz or P286_109B_index2_CGATGT_L001_R1_001.fastq")
+args = parser.parse_args()
 
-Output:
-    a new subdirectory "sanity_check" under the current folder.
-
-"""
-
-if len(sys.argv) < 2:
-        sys.exit(usage)
-
-inFile = sys.argv[1]
-
-parser = optparse.OptionParser()
-parser.add_option('-s', '--subset', type="int", dest="subset", default="5000", help="Number of reads for the subset (default=5000)")
-parser.add_option('-d', '--discard', type="int", dest="startFq", default="25000", help="Number of reads discard from the top (default=25000)")
-parser.add_option('-t', '--num_threads', type="int", dest="threads", default="1", help="Number of threads (CPUs) to use in the BLAST search (default=1)")
-parser.add_option('-n', '--nhit', type="int", dest="nhit", default="50", help="Number of top hits to report (default=50)")
-(opts, args) = parser.parse_args()
-
-subset = opts.subset
-startFq =opts.startFq
-lastFq = subset*2+startFq
-threads = opts.threads
-nhit = opts.nhit
+inFile=args.file_name
+subset = int(args.subset)
+startFq = int(args.startFq)
+lastFq = subset+startFq
+threads = int(opts.threads)
+nhit = int(opts.nhit)
 
 outdir="sanity_check"
 sample=inFile.split('.')[0]
-outFa= outdir+"/tmp/"+sample+".sub"+str(subset)+".fa"
-outFq= outdir+"/tmp/"+sample+".sub"+str(subset)+".fastq"
-outXML= outdir+"/blast_out/"+sample+".sub"+str(subset)+".blast_out.xml"
-resultF= outdir+"/"+sample+".sub"+str(subset)+"_sanityCheck.txt"
+tmpdir=os.path.join(outdir,"tmp")
+xmldir=os.path.join(outdir,"blast_out")
+
+Fa=sample+".sub"+str(subset)+".fa"
+xml=sample+".sub"+str(subset)+".blast_out.xml"
+result=sample+".sub"+str(subset)+"_sanityCheck.txt"
+
+outFa= os.path.join(tmpdir,Fa)
+outXML= os.path.join(xmldir,xml)
+resultF= os.path.join(outdir,result)
 
 if not os.path.exists(outdir):
     os.mkdir(outdir)
-if not os.path.exists(outdir+"/tmp"):
-    os.mkdir(outdir+"/tmp")
-if not os.path.exists(outdir+"/blast_out"):
-    os.mkdir(outdir+"/blast_out")
+if not os.path.exists(tmpdir):
+    os.mkdir(tmpdir)
+if not os.path.exists(xmldir):
+    os.mkdir(xmldir)
 
 if inFile.endswith('.gz'):
     readFq=gzip.open(inFile)
-    rand_records = sorted(random.sample(xrange(startFq,lastFq), subset))
 else:
     readFq=open(inFile)
-    rand_records = sorted(random.sample(xrange(startFq,lastFq), subset))
 
-sub=open(outFq, 'w')
-rec_no = -1
-for rr in rand_records:
+sub=open(outFa, 'w')
+rec_no = 0
+for rr in range(startFq,lastFq):
     while rec_no < rr:
-        for i in range(4):readFq.readline()
+        for i in range(4):
+		readFq.readline()
         rec_no += 1
-    for i in range(4):
-        sub.write(readFq.readline())
+    anno=readFq.readline().replace("@",">")
+    seq=readFq.readline()
+    dump1=readFq.readline()
+    dump2=readFq.readline()
+    sub.write(anno)
+    sub.write(seq)
     rec_no += 1
 sub.close()
 readFq.close()
-
-read = SeqIO.parse(outFq,"fastq")
-handle=open(outFa, "w")
-for record in read:
-	handle.write(">"+record.id+"\n"+str(record.seq)+"\n")
-handle.close()
 
 blast_cmd=NcbiblastnCommandline(query=outFa, db="/bubo/nobackup/uppnex/blast_databases/nt", evalue=0.1, outfmt=5, out=outXML, best_hit_overhang=0.1, best_hit_score_edge=0.1, num_threads=threads, max_target_seqs=1)
 blast_cmd()
@@ -113,4 +98,3 @@ for i in range(0,(nhit+2)):
 	line[1]=str(line[1])
 	outF.write("%s\n" % '\t'.join(line))
 outF.close()
-
