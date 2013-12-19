062ad599d84af3916b3de7048a148411ca490b3c
Maya Brandi <maya.brandi@scilifelab.se>
2013-12-19 09:21:02 +0100

    adding duplication info on top three genes to the analysis database

--- scripts/analysisDB/analysisDB.py
+++ scripts/analysisDB/analysisDB.py
@@ -17,6 +17,7 @@ class BP_RNA():
         self.get_RSeQC()
         self.get_rRNA()
         self.get_picardDup()
+        self.get_top_dups()
 
     def get_proj_db_inf(self, proj_db):
         for key in ['application', 'project_id', 'no_of_samples']:
@@ -24,7 +25,8 @@ class BP_RNA():
                 self.obj[key] = proj_db[key]
 
     def get_stat(self):
-        stat = open('stat.json','r')
+        try:stat = open('stat.json','r')
+        except: return
         stat_dict = eval(stat.read())
         scilife_names,preps = self.strip_scilife_name(stat_dict.keys())
         for samp in scilife_names:
@@ -36,7 +38,8 @@ class BP_RNA():
                 self.obj['samples'][samp_name_stripped] = {'mapping_statistics' : stat_dict[samp],
                                                            'prep': preps[samp]}
     def get_rRNA(self):
-        rRNA = open('rRNA.quantification','r')
+        try:rRNA = open('rRNA.quantification','r')
+        except: return
         for line in rRNA:
             samp = line.split()[0]
             scilife_names, preps = self.strip_scilife_name([samp])
@@ -47,7 +50,8 @@ class BP_RNA():
                 self.obj['samples'][samp_name_stripped] = {'percent_rRNA' : float(line.split()[1].strip('%'))}
 
     def get_RSeQC(self):
-        RSeQC = open('RSeQC_rd.json','r')
+        try:RSeQC = open('RSeQC_rd.json','r')
+        except: return
         RSeQC_dict = eval(RSeQC.read())
         scilife_names,preps = self.strip_scilife_name(RSeQC_dict.keys())
         for samp in scilife_names:
@@ -57,18 +61,36 @@ class BP_RNA():
             else:
                 self.obj['samples'][samp_name_stripped] = {'read_distribution' : RSeQC_dict[samp]}
 
+    def get_top_dups(self):
+        try:top_dups = open('top_dups.json','r')
+        except: return 
+        top_dups_dict = eval(top_dups.read())
+        scilife_names,preps = self.strip_scilife_name(top_dups_dict.keys())
+        for samp in scilife_names:
+            samp_name_stripped = scilife_names[samp]
+            if self.obj['samples'].has_key(samp_name_stripped):
+                self.obj['samples'][samp_name_stripped]['top_dups'] = top_dups_dict[samp]
+            else:
+                self.obj['samples'][samp_name_stripped] = {'top_dups' : top_dups_dict[samp]}
+
+
     def get_picardDup(self):
         names = commands.getoutput("ls -d tophat_out_*|sed 's/tophat_out_//g'").split('\n')
         scilife_names, preps = self.strip_scilife_name(names)
         for samp in scilife_names:
             samp_name_stripped = scilife_names[samp]
-            picardDup = self.pars_picardDup_metrics('tophat_out_' + samp + '/' + samp + '_picardDup_metrics')
+            picardDup = self.pars_picard_metrics('tophat_out_' + samp + '/' + samp + '_picardDup_metrics')
+            picardEstInSize = self.pars_picard_metrics('tophat_out_' + samp + '/' + samp + '.picard_estimated_insert_size')
+            #picardEstInSizeHist= self.pars_picard_histogram('tophat_out_' + samp + '/' + samp + '.picard_estimated_insert_size')
             if self.obj['samples'].has_key(samp_name_stripped):
                 self.obj['samples'][samp_name_stripped]['picard_dup'] = picardDup
+                self.obj['samples'][samp_name_stripped]['picard_estimated_insert_size'] = picardEstInSize
+#                self.obj['samples'][samp_name_stripped]['picard_estimated_insert_size_hist'] = picardEstInSizeHist
             else:
-                self.obj['samples'][samp_name_stripped] = {'picard_dup' : picard_dup}
+                self.obj['samples'][samp_name_stripped] = {'picard_dup' : picard_dup, 'picard_estimated_insert_size' : picardEstInSize} #, 'picard_estimated_insert_size_hist':  picardEstInSizeHist}
 
-    def pars_picardDup_metrics(self,file):
+
+    def pars_picard_metrics(self,file):
         f=open(file,'r')
         lines=f.readlines()
         for i,line in enumerate(lines):
@@ -76,6 +98,21 @@ class BP_RNA():
                 keys=lines[i+1].strip().split('\t')
                 vals=lines[i+2].strip().split('\t')
                 return dict(zip(keys,vals))
+
+    def pars_picard_histogram(self,file):
+        f=open(file,'r')
+        lines=f.readlines()
+        histogram = False
+        for i,line in enumerate(lines):
+            if histogram:
+                values = lines[i+1].strip().split('\t')
+                if values[0] !='':
+                    histogram[keys[0]].append(values[0])
+                    histogram[keys[1]].append(values[1])
+            if len(line.split('\t'))>0 and line.split('\t')[0]=='## HISTOGRAM':
+                keys=lines[i+1].strip().split('\t')
+                histogram = {keys[0]:[], keys[1]:[]}
+        return histogram
 
     def strip_scilife_name(self, names): 
         N = {}
