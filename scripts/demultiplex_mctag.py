0975b3c8a823cacd62954996807719333615bc84
Mario Giovacchini <mario@scilifelab.se>
2014-01-04 19:30:43 +0100

    Initial parsing / writing to files

--- scripts/demultiplex_mctag.py
+++ scripts/demultiplex_mctag.py
@@ -9,11 +9,30 @@ from __future__ import print_function
 
 import argparse
 import Bio
+import collections
+import itertools
+import os
 import re
 import sys
 
-from itertools import izip
-from scilifelab.utils.fastq_utils import FastQParser
+from scilifelab.illumina.hiseq import HiSeqSampleSheet
+from scilifelab.utils.fastq_utils import FastQParser, FastQWriter
+
+class NoIndexMatchException(Exception):
+    def __init__(self, message, *args):
+        if not message:
+            message = "no match to supplied indexes."
+        super(NoIndexMatchException, self).__init__(self, message)
+        #Exception.__init__(self, message)
+        self.args = args
+
+class RevComMatchException(NoIndexMatchException):
+    def __init__(self, message, *args):
+        if not message:
+            message = "no match to supplied indexes but match to reverse complement of supplied indexes. Double-check read direction."
+        Exception.__init__(self, message)
+        self.args = args
+
 
 
 def main(read_one, read_two, read_index, sample_sheet, halo_index_file, halo_index_length, molecular_tag_length=None):
@@ -26,14 +45,35 @@ def parse_readset(read_1_fq, read_2_fq, read_index_fq, halo_index_dict, halo_ind
     """
     Parse input fastq files by index reads.
     """
+    # TODO make a real directory, not this hack for testing
+    output_dir = os.path.join(os.getcwd(), "processed_files")
+    os.makedirs(output_dir)
+
     fqp_1, fqp_2, fqp_ind = map(FastQParser, (read_1_fq, read_2_fq, read_index_fq))
-    for read_1, read_2, read_ind in izip(fqp_1, fqp_2, fqp_ind):
+
+    match, non_match, revcom_match = 0, 0, 0
+
+    for read_1, read_2, read_ind in itertools.izip(fqp_1, fqp_2, fqp_ind):
         # Get the haloplex index and the molecular tag from the index read's sequence data
-        halo_index, molecular_tag, index_name = parse_index(read_ind[1], halo_index_dict, halo_index_revcom_dict,
-                                                            halo_index_length, molecular_tag_length)
+        try:
+            halo_index, molecular_tag = parse_index(read_ind[1], halo_index_dict, halo_index_revcom_dict,
+                                                    halo_index_length, molecular_tag_length)
+            match += 1
+        except RevComMatchException as e:
+            revcom_match += 1
+            halo_index, molecular_tag = e[0], e[1]
+        except NoIndexMatchException as e:
+            non_match += 1
+            halo_index, molecular_tag = e[0], e[1]
         # TODO modify headers of the reads and split by index either in dict, yield, or write to file
+        # possibly use scilifelab.utils.fastq_utils.FastQWriter()
         if halo_index and molecular_tag:
-            print(halo_index, molecular_tag, index_name)
+            for read_num, read in enumerate([read_1, read_2]):
+                read[0] = read[0] + "{}:{}:".format(halo_index, molecular_tag)
+                file_name = os.path.join(output_dir, "{}_R{}.fastq".format(halo_index, read_num+1))
+                with open(file_name, 'a') as f:
+                    f.write("\n".join(read))
+    import pdb; pdb.set_trace()
 
 
 def parse_index(index_seq, halo_index_dict=None, halo_index_revcom_dict=None, halo_index_length=None, molecular_tag_length=None):
@@ -41,16 +81,20 @@ def parse_index(index_seq, halo_index_dict=None, halo_index_revcom_dict=None, ha
     Split an index up into its haloplex index and the random molecular tag.
     Returns the halo index and the molecular tag as a tuple.
     """
-    # TODO add check against known indexes (Bio.pairwise2) -- this will also determine default molecular tag length
-    # TODO add pairwise alignment to known indexes to correct for sequencing errors
     halo_index, molecular_tag = index_seq[:halo_index_length], index_seq[halo_index_length:molecular_tag_length]
+
+    # TODO add check against known indexes (Bio.pairwise2) -- this will also determine default molecular tag length
+    #from Bio import pairwise2 as pw2
+    #    pw2.align.globalms(halo_index, key, 2, -1, -1, -1)
     if not halo_index in halo_index_dict.keys():
         if halo_index in halo_index_revcom_dict.keys():
-            print("WARNING: No match to supplied indexes but match to reverse complement of supplied indexes. Double-check read direction.",
-                  file=sys.stderr)
-        return None, None, None
-    index_name = halo_index_dict[halo_index]
-    return halo_index, molecular_tag, index_name
+            raise RevComMatchException(None, halo_index, molecular_tag)
+        else:
+            raise NoIndexMatchException(None, halo_index, molecular_tag)
+    #    return None, None, None
+    #index_name = halo_index_dict[halo_index]
+    #return halo_index, molecular_tag, index_name
+    return halo_index, molecular_tag
 
 
 def load_indexes(csv_file):
