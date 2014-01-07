5b157715150cac0d14143bb66545478891b33a25
Mario Giovacchini <mario@scilifelab.se>
2014-01-07 10:13:12 +0100

    Various refactoring/improvements, added index file parsing / matching

--- scripts/demultiplex_mctag.py
+++ scripts/demultiplex_mctag.py
@@ -6,7 +6,6 @@ Demultiplex haloplex data including molecular tags.
 from __future__ import print_function
 
 import argparse
-import Bio
 #import collections
 import itertools
 import os
@@ -15,6 +14,7 @@ import shutil
 import subprocess
 import sys
 
+from Bio import Seq, pairwise2
 #from scilifelab.illumina.hiseq import HiSeqSampleSheet
 #from scilifelab.utils.fastq_utils import FastQParser
 
@@ -26,29 +26,36 @@ import sys
 
 
 def main(read_one, read_two, read_index, data_directory, read_index_num, output_directory, halo_index_file, halo_index_length, molecular_tag_length=None, force_overwrite=False):
-#def main(**kwargs):
 
     if not output_directory:
         raise SyntaxError("Must specify output directory.")
-    output_directory = create_output_dir(output_directory, force_overwrite)
+    else:
+        output_directory = create_output_dir(output_directory, force_overwrite)
 
-    # Check for minimum required index information from user
     if halo_index_file:
+        # Load indexes, names from a file
         index_dict, index_revcom_dict = load_index_file(halo_index_file)
     elif halo_index_length:
         assert(type(halo_index_length) == int and halo_index_length > 0), "Haloplex index length must be a positive integer."
         if molecular_tag_length:
             assert(type(molecular_tag_length) == int and molecular_tag_length >= 0), "Molecular tag length must be a postive integer."
+        # else indexes will be extracted from the data itself based on length
         index_dict, index_revcom_dict = None, None
     else:
         raise SyntaxError("Either an index file or the index length must be specified.")
 
-    # Check for minimum required fastq location data from user and process
     if read_one and read_two and read_index:
         if data_directory or read_index_num:
             raise SyntaxError("Ambiguous: too many options specified. Specify either file paths or directory and read index number.")
         else:
-            parse_readset(read_one, read_two, read_index, index_dict, index_revcom_dict, output_directory, halo_index_length, molecular_tag_length)
+            if index_dict:
+                # Indexes are user-supplied
+                reads_processed, num_match, num_nonmatch = parse_readset_byindexdict(read_one, read_two, read_index, index_dict, index_revcom_dict, output_directory)
+                print("\nInfo: Processing complete; {} reads processed ({} matching, {} non-matching).".format(reads_processed, num_match, num_nonmatch), file=sys.stderr)
+            else:
+                # Indexes will be pulled from data itself based on user-supplied length
+                reads_processed = parse_readset(read_one, read_two, read_index, output_directory, halo_index_length, molecular_tag_length)
+                print("\nInfo: Processing complete; {} reads processed..".format(reads_processed), file=sys.stderr)
     elif data_directory and read_index_num:
         parse_directory(data_directory, read_index_num)
         #for readset in parse_directory(directory, read_index_num):
@@ -58,8 +65,86 @@ def main(read_one, read_two, read_index, data_directory, read_index_num, output_
     else:
         raise SyntaxError("Either a directory and read index number or explicit paths to sequencing files must be specified.")
 
-    print("Info: Processing complete.", file=sys.stderr)
 
+
+# TODO turn this into a generator and use it in a for loop above
+def parse_directory(data_directory, read_index_num):
+    """
+    Searches the directory for fastq file sets and calls parse_readset() on them.
+    """
+    raise NotImplementedError("I haven't implemented this yet, so don't go using it.")
+
+# TODO parallelize
+# TODO add checking against reverse_complement sequences
+# TODO there's probably a good way to combine these two parse_readset functions
+def parse_readset_byindexdict(read_1_fq, read_2_fq, read_index_fq, index_dict, index_revcom_dict, output_directory):
+    """
+    Parse input fastq files, searching for matches to each index.
+    """
+
+    fqp_1, fqp_2, fqp_ind = map(FastQParser, (read_1_fq, read_2_fq, read_index_fq))
+
+    #match, non_match, revcom_match = 0, 0, 0
+    reads_processed, num_match, num_nonmatch = 0, 0, 0
+
+
+    for read_1, read_2, read_ind in itertools.izip(fqp_1, fqp_2, fqp_ind):
+        read_ind_seq = read_ind[1]
+        for supplied_index in index_dict.keys():
+            if re.match(supplied_index, read_ind_seq):
+                index_len = len(supplied_index)
+                index, molecular_tag = read_ind_seq[:index_len], read_ind_seq[index_len:]
+                num_match += 1
+
+                read_list = [read_1, read_2]
+                modify_reads(read_list, index, molecular_tag)
+                sample_name = index_dict[supplied_index] if index_dict[supplied_index] else supplied_index
+                write_reads_to_disk(read_list, sample_name, output_directory)
+
+                break
+            else:
+                num_nonmatch += 1
+        reads_processed += 1
+
+        if reads_processed % 100 == 0:
+            print("Processed {} reads...".format(reads_processed), file=sys.stderr)
+
+    print("Processed {} reads, processing complete.".format(reads_processed), file=sys.stderr)
+    if num_match == 0:
+        print("\nWarning: no reads matched supplied indexes; nothing written.", file=sys.stderr)
+    return reads_processed, num_match, num_nonmatch
+
+# TODO there's probably a good way to combine these two parse_readset functions
+def parse_readset(read_1_fq, read_2_fq, read_index_fq, output_directory, halo_index_length, molecular_tag_length=None):
+    """
+    Parse input fastq files by index reads.
+    """
+    fqp_1, fqp_2, fqp_ind = map(FastQParser, (read_1_fq, read_2_fq, read_index_fq))
+
+    #match, non_match, revcom_match = 0, 0, 0
+    reads_processed, reads_match, reads_nonmatch = 0, 0, 0
+
+    for read_1, read_2, read_ind in itertools.izip(fqp_1, fqp_2, fqp_ind):
+        read_ind_seq = read_ind[1]
+        index, molecular_tag = read_ind_seq[:halo_index_length], read_ind_seq[halo_index_length:molecular_tag_length] 
+
+        read_list = [read_1, read_2]
+        modify_reads(read_list, index, molecular_tag)
+        sample_name = index
+        write_reads_to_disk(read_list, sample_name, output_directory)
+
+        reads_processed += 1
+        if reads_processed % 100 == 0:
+            print("Processed {} reads...".format(reads_processed), file=sys.stderr)
+
+    return reads_processed
+
+def modify_reads(read_list, index, molecular_tag):
+    """
+    Add the extra tags to the header of each read
+    """
+    for read in read_list:
+        read[0] = read[0] + "{}:{}:".format(index, molecular_tag)
 
 def create_output_dir(output_directory, force_overwrite):
     """
@@ -75,65 +160,16 @@ def create_output_dir(output_directory, force_overwrite):
     os.makedirs(output_directory)
     return output_directory
 
-# TODO turn this into a generator and use it in a for loop above
-#def parse_directory(data_directory, read_index_num, index_dict, index_revcom_dict, halo_index_length, molecular_tag_length):
-def parse_directory(data_directory, read_index_num):
+# TODO see if it's faster to keep a dict of { index -> filehandle }
+def write_reads_to_disk(read_list, sample_name, output_directory):
     """
-    Searches the directory for fastq file sets and calls parse_readset() on them.
+    Write a Fastq read to the appropriate file.
     """
-    raise NotImplementedError("I haven't implemented this yet, so don't go using it.")
-
-def parse_readset(read_1_fq, read_2_fq, read_index_fq, index_dict, index_revcom_dict, output_directory, halo_index_length, molecular_tag_length=None, force_append=False):
-    """
-    Parse input fastq files by index reads.
-    """
-
-    fqp_1, fqp_2, fqp_ind = map(FastQParser, (read_1_fq, read_2_fq, read_index_fq))
-
-    match, non_match, revcom_match = 0, 0, 0
-
-    for read_1, read_2, read_ind in itertools.izip(fqp_1, fqp_2, fqp_ind):
-        # Get the haloplex index and the molecular tag from the index read's sequence data
-        try:
-            halo_index, molecular_tag = parse_index(read_ind[1], index_dict, index_revcom_dict,
-                                                    halo_index_length, molecular_tag_length)
-            match += 1
-        except RevComMatchException as e:
-            revcom_match += 1
-            halo_index, molecular_tag = e[0], e[1]
-        except NoIndexMatchException as e:
-            non_match += 1
-            halo_index, molecular_tag = e[0], e[1]
-        # TODO modify headers of the reads and split by index either in dict, yield, or write to file
-        # possibly use scilifelab.utils.fastq_utils.FastQWriter()
-        if halo_index and molecular_tag:
-            for read_num, read in enumerate([read_1, read_2]):
-                read[0] = read[0] + "{}:{}:".format(halo_index, molecular_tag)
-                file_name = os.path.join(output_directory, "{}_R{}.fastq".format(halo_index, read_num+1))
-                with open(file_name, 'a') as f:
-                    f.write("\n".join(read) + "\n")
-
-
-# TODO add checking against a file
-def parse_index(index_seq, index_dict=None, index_revcom_dict=None, halo_index_length=None, molecular_tag_length=None):
-    """
-    Split an index sequence up into its haloplex index and the random molecular tag.
-    Returns the halo index and the molecular tag as a tuple.
-    """
-    halo_index, molecular_tag = index_seq[:halo_index_length], index_seq[halo_index_length:molecular_tag_length]
-
-    # TODO add check against known indexes (Bio.pairwise2) -- this will also determine default molecular tag length
-    #from Bio import pairwise2 as pw2
-    #    pw2.align.globalms(halo_index, key, 2, -1, -1, -1)
-    #if not halo_index in index_dict.keys():
-    #    if halo_index in index_revcom_dict.keys():
-    #        raise RevComMatchException(None, halo_index, molecular_tag)
-    #    else:
-    #        raise NoIndexMatchException(None, halo_index, molecular_tag)
-    #    return None, None, None
-    #index_name = index_dict[halo_index]
-    #return halo_index, molecular_tag, index_name
-    return halo_index, molecular_tag
+    for read_num, read in enumerate(read_list):
+        file_name = "{sample_name}_R{read_num}.fastq".format(sample_name=sample_name, read_num=read_num+1)
+        file_name = os.path.join(output_directory, file_name)
+        with open(file_name, 'a') as f:
+            f.write("\n".join(read) + "\n")
 
 
 def load_index_file(csv_file):
@@ -151,7 +187,7 @@ def load_index_file(csv_file):
             # could also use csv.sniffer to dynamically determine delimiter
             index = re.split(r'[\t,;]', line.strip())
             # include reverse complement
-            rev_com_index = Bio.Seq.Seq(index[0]).reverse_complement().tostring()
+            rev_com_index = Seq.Seq(index[0]).reverse_complement().tostring()
             try:
                 index_dict[index[0]]                = index[1]
                 index_dict_revcom[rev_com_index]    = index[1]
@@ -161,13 +197,13 @@ def load_index_file(csv_file):
     return index_dict, index_dict_revcom
 
 # I'm just putting this class here temporarily so I can test this without needing to load in all the other scilifelab jazz
-class FastQParser:
-    """Parser for fastq files, possibly compressed with gzip. 
-       Iterates over one record at a time. A record consists 
-       of a list with 4 elements corresponding to 1) Header, 
+class FastQParser(object):
+    """Parser for fastq files, possibly compressed with gzip.
+       Iterates over one record at a time. A record consists
+       of a list with 4 elements corresponding to 1) Header,
        2) Nucleotide sequence, 3) Optional header, 4) Qualities"""
 
-    def __init__(self,file,filter=None):
+    def __init__(self, file, filter=None):
         self.fname = file
         self.filter = filter
 
@@ -217,6 +253,27 @@ class FastQParser:
 
     def close(self):
         self._fh.close()
+
+def parse_header(header):
+    """Parses the FASTQ header as specified by CASAVA 1.8.2 and returns the fields in a dictionary
+       @<instrument>:<run number>:<flowcell ID>:<lane>:<tile>:<x-pos>:<y-pos> <read>:<is filtered>:<control number>:<index sequence>
+    """
+    if header[0] != '@':
+        return None
+
+    instrument, run_number, flowcell_id, lane, tile, x_pos, y_pos_read, is_filtered, control_number, index = header[1:].split(":")
+    y_pos, read = y_pos_read.split()
+    return {'instrument': str(instrument),
+            'run_number': int(run_number),
+            'flowcell_id': str(flowcell_id),
+            'lane': int(lane),
+            'tile': int(tile),
+            'x_pos': int(x_pos),
+            'y_pos': int(y_pos),
+            'read': int(read),
+            'is_filtered': (is_filtered == 'Y'),
+            'control_number': int(control_number),
+            'index': str(index)} # Note that MiSeq Reporter outputs a SampleSheet index rather than the index sequence
 
 # TODO these classes don't print the correct error message - maybe a problem with super? review docs
 class NoIndexMatchException(Exception):
@@ -268,8 +325,6 @@ if __name__ == "__main__":
 
     # TODO parser.add_argument("-s", "--single-read", action="store_true", help="Specify that the data is single-read (not paired-end). Default false.")
 
-    # TODO ensure exclusive subcommands for indexes, fastq data: check namespaces
-    # TODO set unset variables to None? or check for presence of variables here
     arg_vars = vars(parser.parse_args())
 
     main(**arg_vars)
