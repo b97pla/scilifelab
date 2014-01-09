bd0b8700a8a78ac81efba3fe7545ed17ac25e7a8
Mario Giovacchini <mario.giovacchini@scilifelab.se>
2014-01-09 12:10:20 +0100

    Code cleanup

--- scripts/demultiplex_mctag.py
+++ scripts/demultiplex_mctag.py
@@ -25,40 +25,41 @@ import time
 # TODO ensure read 1,2 files are paired (SciLifeLab code)
 # TODO add directory processing
 
-
 def main(read_one, read_two, read_index, data_directory, read_index_num, output_directory, index_file, force_overwrite=False):
-
-    if not output_directory:
-        raise SyntaxError("Must specify output directory.")
-    else:
-        output_directory = create_output_dir(output_directory, force_overwrite)
-    if not index_file:
-        raise SyntaxError("Must specify file containing indexes.")
-    else:
-        # TODO NOTE: I changed this just for Jimmy's run
-        index_dict = load_index_file(index_file, usecols=(2,1))
-
+    check_input(read_one, read_two, read_index, data_directory, read_index_num, output_directory, index_file)
+    output_directory = create_output_dir(output_directory, force_overwrite)
+    # TODO NOTE: I changed this just for Jimmy's run
+    index_dict = load_index_file(index_file, usecols=(2,1))
     if read_one and read_two and read_index:
-        if data_directory or read_index_num:
-            raise SyntaxError("Ambiguous: too many options specified. Specify either file paths or directory and read index number.", file=sys.stderr)
-        else:
-            print("Processing read set associated with \"{}\" using user-supplied indexes.".format(read_one), file=sys.stderr)
-            reads_processed, num_match, num_nonmatch = parse_readset_byindexdict(read_one, read_two, read_index, index_dict, output_directory)
-    elif data_directory and read_index_num:
+        reads_processed, num_match, num_nonmatch = parse_readset_byindexdict(read_one, read_two, read_index, index_dict, output_directory)
+    else:
         for readset in parse_directory(data_directory, read_index_num):
             read_one, read_two, read_index = readset
             reads_processed, num_match, num_nonmatch = parse_readset_byindexdict(read_one, read_two, read_index, index_dict, output_directory)
-    else:
-        raise SyntaxError("Insufficient information: either a directory and read index number or explicit paths to sequencing files must be specified.")
-
-    num_match_percent       = (100.0 * num_match)/reads_processed
-    num_nonmatch_percent    = (100.0 * num_nonmatch)/reads_processed
-    pad_length              = len(str(reads_processed))
-    print(  "\nProcessing complete:\n\t{reads_processed} reads processed\n\t" \
+    print(  "\nProcessing complete:\n\t" \
+            "{reads_processed} reads processed\n\t" \
             "{num_match:>{pad_length}} ({num_match_percent:>6.2f}%) matched to supplied indexes\n\t" \
             "{num_nonmatch:>{pad_length}} ({num_nonmatch_percent:>6.2f}%) unmatched to supplied indexes".format(
-                reads_processed=reads_processed, num_match=num_match, num_match_percent=num_match_percent,
-                num_nonmatch=num_nonmatch, num_nonmatch_percent=num_nonmatch_percent, pad_length=pad_length), file=sys.stderr)
+                reads_processed=reads_processed, num_match=num_match, num_nonmatch=num_nonmatch,
+                num_match_percent   = (100.0 * num_match)/reads_processed,
+                num_nonmatch_percent= (100.0 * num_nonmatch)/reads_processed,
+                pad_length = len(str(reads_processed))), file=sys.stderr)
+
+
+def check_input(read_one, read_two, read_index, data_directory, read_index_num, output_directory, index_file):
+    """
+    Check user-supplied inputs for validity, completeness.
+    """
+    if not output_directory:
+        raise SyntaxError("Must specify output directory.")
+    if not index_file:
+        raise SyntaxError("Must specify file containing indexes.")
+    if not (read_one and read_index):
+        raise SyntaxError("Must speify both data and index reads.")
+    if (read_one or read_two or read_index) and (data_directory or read_index_num):
+        raise SyntaxError("Ambiguous: too many options specified. Specify either file paths or directory and read index number.")
+    if not (read_one and read_two and read_index) or (data_directory and read_index_num):
+        raise SyntaxError("Insufficient information: either a directory and read index number or explicit paths to sequencing files must be specified.")
 
 
 def parse_directory(data_directory, read_index_num):
@@ -73,43 +74,33 @@ def parse_readset_byindexdict(read_1_fq, read_2_fq, read_index_fq, index_dict, o
     """
     Parse input fastq files, searching for matches to each index.
     """
-
-    fqp_1, fqp_2, fqp_ind = map(FastQParser, (read_1_fq, read_2_fq, read_index_fq))
-
+    print("Processing read set associated with \"{}\" using user-supplied indexes.".format(read_1_fq), file=sys.stderr)
     reads_processed, num_match, num_nonmatch = 0, 0, 0
-
+    fqp_1, fqp_2, fqp_ind = map(FastQParser, (read_1_fq, read_2_fq, read_index_fq))
     print("Counting total number of lines in fastq files...", file=sys.stderr, end="")
-    # TODO Actually I think du -k * 16 / 1.024 should give approximately the right number for any number of reads greater than 1000 or so
+    # I think du -k * 16 / 1.024 should give approximately the right number for any number of reads greater than 1000 or so
     total_lines_in_file = int(subprocess.check_output(shlex.split("wc -l {}".format(read_1_fq))).split()[0])
-    #total_lines_in_file = sum(1 for line in open(read_1_fq))
     print(" complete.", file=sys.stderr)
-
-    time_started = datetime.datetime.now()
     print("Demultiplexing...", file=sys.stderr)
+    time_started = datetime.datetime.now()
     for read_1, read_2, read_ind in itertools.izip(fqp_1, fqp_2, fqp_ind):
         read_ind_seq = read_ind[1]
-        read_list = [read_1, read_2]
         for supplied_index in index_dict.keys():
             if re.match(supplied_index, read_ind_seq):
                 index_len = len(supplied_index)
                 index, molecular_tag = read_ind_seq[:index_len], read_ind_seq[index_len:]
                 num_match += 1
-
-                modify_reads(read_list, index, molecular_tag)
+                modify_reads( (read_1, read_2), index, molecular_tag)
                 sample_name = index_dict[supplied_index] if index_dict[supplied_index] else supplied_index
-                write_reads_to_disk(read_list, sample_name, output_directory)
-
+                write_reads_to_disk( (read_1, read_2), sample_name, output_directory)
                 break
         else:
             sample_name = "Undetermined"
-            write_reads_to_disk(read_list, sample_name, output_directory)
+            write_reads_to_disk( (read_1, read_2), sample_name, output_directory)
             num_nonmatch += 1
         reads_processed += 1
-        # TODO only update every 0.1% or something maybe -- is this slowing things down?
-        print_progress(reads_processed, (total_lines_in_file / 4), time_started=time_started)
-
-    if num_match == 0:
-        print("\nWarning: no reads matched supplied indexes; nothing written.", file=sys.stderr)
+        if reads_processed % 100 == 0:
+            print_progress(reads_processed, (total_lines_in_file / 4), time_started=time_started)
     return reads_processed, num_match, num_nonmatch
 
 
@@ -117,25 +108,21 @@ def print_progress(processed, total, type='text', leading_text="", time_started=
     """
     Prints the progress, either in text or in visual form.
     """
-    # TODO switch this back to a decimal < 1
-    percentage_complete = 100 * (float(processed) / total)
-
+    percentage_complete = float(processed) / total
     if time_started:
-        completion_time = estimate_completion_time(time_started, (percentage_complete / 100))
+        completion_time = estimate_completion_time(time_started, percentage_complete)
     else:
         time_started = "-"
-
     _, term_cols = os.popen('stty size', 'r').read().split()
     term_cols = int(term_cols) - 10 - len(leading_text)
-
     sys.stderr.write('\r')
     if type == 'bar' and term_cols > 10:
-        progress = int((term_cols * percentage_complete / 100))
+        progress = int(term_cols * percentage_complete)
         sys.stderr.write("{leading_text}[{progress:<{cols}}] {percentage:0.2f}%".format(
-            leading_text=leading_text, progress="="*progress, cols=term_cols, percentage=percentage_complete))
+            leading_text=leading_text, progress="="*progress, cols=term_cols, percentage=percentage_complete*100))
     else:
         sys.stderr.write("{leading_text}{processed}/{total} items processed ({percentage_complete:0.2f}% finished) ETA: {completion_time}".format(
-            leading_text=leading_text, processed=processed, total=total, percentage_complete=percentage_complete, completion_time=completion_time))
+            leading_text=leading_text, processed=processed, total=total, percentage_complete=percentage_complete*100, completion_time=completion_time))
     sys.stderr.flush()
 
 def estimate_completion_time(start_time, percent_complete):
@@ -145,14 +132,12 @@ def estimate_completion_time(start_time, percent_complete):
     """
     if not type(start_time) == datetime.datetime:
         return None
-
     seconds_elapsed = (datetime.datetime.now() - start_time).total_seconds()
     seconds_total = seconds_elapsed / percent_complete
     seconds_left = seconds_total - seconds_elapsed
-
     # more than a day remaining
     if seconds_left > 86400:
-        days    = seconds_left // 86400
+        days    = int(seconds_left // 86400)
         seconds = seconds_left - (days * 86400)
         return "{}:{}".format(days, time.strftime('%H:%M:%S', time.gmtime(seconds)))
     else:
@@ -164,16 +149,12 @@ def count_top_indexes(count_num, index_file, index_length):
     Determine the most common indexes, sampling at most 200,000 reads.
     """
     assert(type(count_num) == int and count_num > 0), "Number passed must be a positive integer."
-
     fqp_ind = FastQParser(index_file)
-
     # This should perhaps be added to the FastQParser class
     print("Counting total number of lines in fastq file...", file=sys.stderr, end="")
     total_lines = int(subprocess.check_output(shlex.split("wc -l {}".format(index_file))).split()[0])
     total_reads = total_lines / 4
-    #total_lines = sum(1 for line in open(index_file))
     print(" complete.", file=sys.stderr)
-
     index_tally = collections.defaultdict(int)
     processed_reads = 0
     # Subsample if file is large
@@ -182,22 +163,19 @@ def count_top_indexes(count_num, index_file, index_length):
         fqp_ind = iter_sample_fast(fqp_ind, 200000, total_reads)
         print("Complete.", file=sys.stderr)
         total_reads = 200000
-
     print("Tallying indexes in {} records...".format(total_reads), file=sys.stderr)
+    start_time = datetime.datetime.now()
     for index in fqp_ind:
         index_read_seq = index[1]
         index_seq = index_read_seq[:index_length]
         index_tally[index_seq] += 1
         processed_reads += 1
-
-        print_progress(processed_reads, total_reads)
+        print_progress(processed_reads, total_reads, start_time)
     print("\n", file=sys.stderr)
-
     if count_num > len(index_tally.keys()):
         print("Number of indexes found ({}) is fewer than those requested ({}). Printing all indexes found.".format(len(index_tally.keys()), count_num), file=sys.stderr)
         print("Printing indexes...", file=sys.stderr())
         count_num = len(index_tally.keys())
-
     print("{:<20} {:>20} {:>11}".format("Index", "Occurences", "Percentage"))
     for index, _ in sorted(index_tally.items(), key=(lambda x: x[1]), reverse=True)[:count_num]:
         percentage = (100.0 * index_tally[index] ) / total_reads
@@ -244,7 +222,6 @@ def create_output_dir(output_directory, force_overwrite):
             print("Warning: removing/overwriting output directory \"{}\"...".format(output_directory), file=sys.stderr, end="")
             shutil.rmtree(output_directory)
             print(" removed.", file=sys.stderr)
-    # This can be outside the conditional because I don't want to raise my own exception in case of failure
     os.makedirs(output_directory)
     return output_directory
 
@@ -270,20 +247,18 @@ def load_index_file(csv_file, usecols=(0,1)):
     """
     if not len(usecols) == 2:
         raise ValueError("Warning: only two columns (index, sample_name) can be selected.", file=sys.stderr)
-
     if not usecols == (0,1):
         print("Warning: using non-standard columns for index, sample_name ({} instead of (0,1))".format(usecols), file=sys.stderr)
     index_ind, name_ind = usecols
     index_dict          = {}
-
     with open(csv_file, 'r') as f:
         for line in f:
             # could also use csv.sniffer to dynamically determine delimiter
             index = re.split(r'[\t,;]', line.strip())
             try:
-                index_dict[index[index_ind]]                = index[name_ind]
+                index_dict[index[index_ind]] = index[name_ind]
             except IndexError:
-                index_dict[index[index_ind]]                = None
+                index_dict[index[index_ind]] = None
     return index_dict
 
 # SCILIFELAB CODE
@@ -376,33 +351,25 @@ if __name__ == "__main__":
     parser.add_argument("-i", "--index-file",
                                 help="File containing haloplex indexes (one per line, optional name " \
                                      "in second column separated by tab, comma, or semicolon).")
-
     parser.add_argument("-1", "--read-one",
                                 help="Read 1 fastq file.")
     parser.add_argument("-2", "--read-two",
                                 help="Read 2 fastq file.")
     parser.add_argument("-r", "--read-index",
                                 help="Index read fastq file.")
-
     parser.add_argument("-d", "--data-directory",
                                 help="Directory containing fastq read data. Requires specifying read number (-n).")
     parser.add_argument("-n", "--read-index-num", type=int,
                                 help="Which read is the index (e.g. 1, 2, 3).")
-
     parser.add_argument("-f", "--force-overwrite", action="store_true",
                                 help="Force overwrite to output directory.")
-
     parser.add_argument("-t", "--top-indexes", type=int,
                                 help="Find the n most common indexes. Pair with -l (index length) and -r (read index file). Does not perform any demultiplexing.")
-    parser.add_argument( "-l", "--index-length", type=int,
+    parser.add_argument("-l", "--index-length", type=int,
                                 help="The length of the index.")
-
     arg_vars = vars(parser.parse_args())
-
-
     # It's my namespace and I'll clobber it if I want to
     locals().update(arg_vars)
-
     if arg_vars['top_indexes']:
         if not arg_vars['index_length']:
             raise SyntaxError("Must indicate index length to tally.")
