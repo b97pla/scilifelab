0ea4e0a91159f2aa427aefa6db8eea0d4e9c0a53
Jun Wang <junw@milou-b.uppmax.uu.se>
2014-07-14 15:39:21 +0200

    add comments

--- scripts/RNA_analysis/GenerateComplexityPlots.sh
+++ scripts/RNA_analysis/GenerateComplexityPlots.sh
@@ -15,9 +15,11 @@ wp=$1
 
 unset num
 unset filelist
+##Get to file list and the 'total_reads' in *.ccurve.txt
 space=" "
 for files in `ls tophat_out_*/*.ccurve.txt | sort`; do filelist+=($files$space); n=`tail -n 1 ${files}|cut -f 1`; num+=($n$space); done;
 asize=${#filelist[@]}
+##set x-max value, should be min(total_reads) of all samples
 xmax=`echo ${num[@]} |sed 's/ /\n/g' |sort -n|sed -n '1p'`
 
 python ${wp}/plot_complexity_curves.py --ccurves ${filelist[@]} --x-max ${xmax} 
